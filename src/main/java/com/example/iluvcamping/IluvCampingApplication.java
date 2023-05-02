2d4b7710e7d5f54730a21fba17d178ba8b0ef671
jjamong <jieun98514@gmail.com>
2023-05-02 17:28:10 +0900

    Create IluvCamping project

---
+++ src/main/java/com/example/iluvcamping/IluvCampingApplication.java
@@ -0,0 +1,13 @@
+package com.example.iluvcamping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IluvCampingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IluvCampingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iluvcamping/IluvCampingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iluvcamping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IluvCampingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
