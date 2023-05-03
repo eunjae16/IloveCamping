3d2cd5539b4f01e2c2d5ee6ac19572ab33b0f7ea
eunjae16 <eunjae16@naver.com>
2023-05-03 12:33:48 +0900

    update OwnerService.java 조은재

--- src/main/java/com/example/iluvcamping/service/OwnerService.java
+++ src/main/java/com/example/iluvcamping/service/OwnerService.java
@@ -1,10 +1,12 @@
 package com.example.iluvcamping.service;
 
+import com.example.iluvcamping.domain.client.Client;
 import com.example.iluvcamping.domain.owner.Owner;
 import com.example.iluvcamping.domain.owner.OwnerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -15,6 +17,10 @@ public class OwnerService {
 
     public void addOwner(Owner owner) {
         ownerRepository.save(owner);
+    }
+
+    public List<Owner> getOwnerAllByIdAndPassword(String id, String password){
+        return ownerRepository.getAllByOwnerIdAndOwnerPassword(id, password);
     }
 
     public Optional<Owner> getOwner(String id) {
