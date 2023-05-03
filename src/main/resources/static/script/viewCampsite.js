3afcd62eba84d16abce5912e6af031d32da0c958
eunjae16 <eunjae16@naver.com>
2023-05-03 10:10:10 +0900

    update viewCampsite.js 조은재

--- src/main/resources/static/script/viewCampsite.js
+++ src/main/resources/static/script/viewCampsite.js
@@ -1,22 +1,26 @@
-// select 요소 변경 시, 서버에 캠핑장 목록 요청 후 결과를 화면에 표시하는 함수
 function searchCamp() {
-    var selectedOption = document.querySelector('select[name="selectSite"]').value;
+    var selectedOption = document.querySelector('#selectSite').value;
 
-    fetch('/searchCamp?selectedOption=' + selectedOption)
+    fetch('/getCampList?selectedValue=' + selectedOption)
         .then(response => response.json())
         .then(data => {
             var campListDiv = document.getElementById('campList');
             campListDiv.innerHTML = ''; // 기존 목록 초기화
 
             // 캠핑장 목록을 순회하며 각각의 속성을 화면에 표시
-            data.forEach(camp => {
-                var campDiv = document.createElement('div');
-                campDiv.innerText = '캠핑장 이름: ' + camp.campName + ', 주소: ' + camp.campAddress1;
-                campListDiv.appendChild(campDiv);
-            });
+            if (Array.isArray(data)) { // 배열인지 확인
+                data.forEach(camp => {
+                    var campDiv = document.createElement('div');
+                    campDiv.textContent = '캠핑장 이름: ' + camp.campName + ', 주소: ' + camp.campAddress1;
+                    campListDiv.appendChild(campDiv);
+                });
+            }
+            else {
+                console.log('서버에서 받은 응답이 배열이 아닙니다.');
+            }
         })
         .catch(error => console.log(error));
 }
 
 // select 요소의 변경 이벤트에 searchCamp 함수 연결
-document.querySelector('select[name="selectSite"]').addEventListener('change', searchCamp);
+document.querySelector('#selectSite').addEventListener('change', searchCamp);
