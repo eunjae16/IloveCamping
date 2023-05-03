24c72728c61ac2061a4877fc70e7ef2b85357dfe
eunjae16 <eunjae16@naver.com>
2023-05-03 18:53:15 +0900

    update CampService.java 조은재

--- src/main/java/com/example/iluvcamping/service/CampService.java
+++ src/main/java/com/example/iluvcamping/service/CampService.java
@@ -3,6 +3,8 @@ package com.example.iluvcamping.service;
 
 import com.example.iluvcamping.domain.camp.Camp;
 import com.example.iluvcamping.domain.camp.CampRepository;
+import com.example.iluvcamping.domain.campThemView.CampThemeView;
+import com.example.iluvcamping.domain.campThemView.CampThemeViewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,12 @@ import java.util.List;
 @Service
 public class CampService {
     public final CampRepository campRepository;
+    public final CampThemeViewRepository campThemeViewRepository;
 
     public List<Camp> getCampAll(){
         return campRepository.findAll();
+    }
+    public List<CampThemeView> getViewAllById(List<String> ids){
+        return campThemeViewRepository.findAllById(ids);
     }
 }
