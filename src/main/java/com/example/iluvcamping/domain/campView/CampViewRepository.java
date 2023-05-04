94fcce0ac7d3a482ec24ce5ed29adc979db630e8
eunjae16 <eunjae16@naver.com>
2023-05-04 16:54:30 +0900

    update CampViewRepository.java 조은재

--- src/main/java/com/example/iluvcamping/domain/campView/CampViewRepository.java
+++ src/main/java/com/example/iluvcamping/domain/campView/CampViewRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CampViewRepository extends JpaRepository<CampView, String> {
-    List<CampView> findByCampAddress1StartingWith(String addressPrefix);
+    List<CampView> findAllByCampAddress1StartingWith(String addressPrefix);
 }
